e94a5a2fb3ba84338e92fef90ff8496a8cc72b95
victorimannuel <victorimannuel.dev@gmail.com>
2024-09-23 22:47:31 +0700

    feat(report): add new module report
    - to facilitate all ablepack report related feature/modification

---
+++ addons/abp_report/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Ablepack Report',
+    'category': '',
+    'sequence': 23,
+    'summary': 'Ablepack Report',
+    'version': '1.0',
+    'description': """
+    """,
+    'depends': [
+        # 'sale',
+    ],
+    'data': [
+        'reports/product_barcode.xml',
+        # 'reports/sale/ir_actions_report_templates.xml',
+        # 'reports/sale/ir_actions_report.xml',
+    ],
+    'assets': {
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'LGPL-3',
+}
