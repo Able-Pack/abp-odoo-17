95dec97714101e33c96dab6be3456025e1591fae
victorimannuel <victorimannuel.dev@gmail.com>
2024-09-27 01:18:00 +0700

    feat: add new module
    - commission module

---
+++ external_addons/bi_emp_sales_target_ent/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+from . import hr_employee
+from . import hr_payslip
+from . import sale_commission
+from . import sale_target
