9fa229b193f4594f68b8e767a99d28d6cdc0402a
victorimannuel <victorimannuel.dev@gmail.com>
2024-09-27 23:41:21 +0700

    feat: add new module
    - salesperson dashboard to accomodate all the needs of able pack's salesperson

---
+++ addons/abp_salesperson_dashboard/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Able Pack Salesperson Dashboard',
+    'category': '',
+    'sequence': 23,
+    'author': 'Victor (Child of God)',
+    'summary': 'Able Pack Salesperson Dashboard',
+    'version': '17.0',
+    'description': """
+        A module that accomodate all the needs of Able Pack's salesperson
+    """,
+    'depends': [
+        'sale',
+        'sales_team',
+    ],
+    'data': [
+        'views/menu_items.xml',
+    ],
+    'assets': {
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3',
+}
