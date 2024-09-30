2f50af75dcd7cec1d94f744d8fe8d5a51d0ea09b
victorimannuel <victorimannuel.dev@gmail.com>
2024-09-30 23:08:33 +0700

    feat(pricelist): add pricelist item computation based on retail price

--- addons/abp_pricelist/models/pricelist_item.py
+++ addons/abp_pricelist/models/pricelist_item.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import api, fields, models, tools
 
 
 class PricelistItem(models.Model):
@@ -6,6 +6,9 @@ class PricelistItem(models.Model):
     
     barcode = fields.Char(string='EAN13')
     label_qty = fields.Integer(string='Label Qty')
+    retail_price = fields.Monetary()
+    distributor_price = fields.Monetary(compute='_compute_distributor_price')
+    base = fields.Selection(selection_add=[('retail_price', 'Retail Price')], ondelete={'retail_price': 'cascade'})
     
     # Will be called from tree views
     def button_print_barcode(self):
@@ -23,3 +26,22 @@ class PricelistItem(models.Model):
             })
         return data
     
+    @api.depends('retail_price', 'price_discount', 'price_surcharge', 'price_round')
+    def _compute_distributor_price(self):
+        for rec in self:
+            discounted_price = rec.retail_price * (100-rec.price_discount) / 100
+            extra_price = rec.price_surcharge
+            computed_price = discounted_price + extra_price
+            if rec.price_round:
+                computed_price = tools.float_round(computed_price, precision_rounding=rec.price_round)
+            rec.distributor_price = computed_price
+    
+    def _compute_base_price(self, product, quantity, uom, date, currency):
+        for rec in self:
+            # Directly use retail price if the type is pricelist item is based on Retail Price
+            if rec.base == 'retail_price':
+                # Return this so that it will automatically computed in sale order
+                price = rec.retail_price
+                return price
+            else:
+                return super()._compute_base_price(product, quantity, uom, date, currency)
