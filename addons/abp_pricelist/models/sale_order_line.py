225fa358de1a201c588e60592acfa2305e8f4468
victorimannuel <victorimannuel.dev@gmail.com>
2024-10-02 09:42:10 +0700

    fix: comment unused code

--- addons/abp_pricelist/models/sale_order_line.py
+++ addons/abp_pricelist/models/sale_order_line.py
@@ -8,11 +8,11 @@ class SaleOrderLine(models.Model):
     retail_price = fields.Monetary()
     
     # Override parent method
-    @api.depends('product_id', 'product_uom', 'product_uom_qty')
-    def _compute_pricelist_item_id(self):
-        for rec in self:
-            super()._compute_pricelist_item_id()
-            if rec.pricelist_item_id:
-                rec.barcode = rec.pricelist_item_id.barcode
-                rec.retail_price = rec.pricelist_item_id.retail_price
+    # @api.depends('product_id', 'product_uom', 'product_uom_qty')
+    # def _compute_pricelist_item_id(self):
+    #     for rec in self:
+    #         super()._compute_pricelist_item_id()
+    #         if rec.pricelist_item_id:
+    #             rec.barcode = rec.pricelist_item_id.barcode
+    #             rec.retail_price = rec.pricelist_item_id.retail_price
                 
