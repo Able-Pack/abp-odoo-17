da311985acfb89c905af3eccaa7687cfb9bbc4ef
victorimannuel <victorimannuel.dev@gmail.com>
2024-10-21 10:01:25 +0700

    fix(customer catalogue): re-show customer product ref
    - in customer catalogue tree view
    - in pricelist item form and tree view

--- addons/abp_customer_catalogue/models/__init__.py
+++ addons/abp_customer_catalogue/models/__init__.py
@@ -1,9 +1,9 @@
 from . import customer_catalogue
-from . import product_template
+from . import pricelist
 from . import product_product
+from . import product_template
 from . import res_partner
-from . import sale_order_line
 from . import sale_order
-from . import stock_picking
+from . import sale_order_line
 from . import stock_move
-
+from . import stock_picking
