93966e38b392918e786c70f25f411898f4e55eb7
victorimannuel <victorimannuel.dev@gmail.com>
2024-11-04 02:46:42 +0700

    feat(inventory): add a button to print delivery slip
    - in stock picking form view

---
+++ addons/abp_inventory/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Able Pack Inventory',
+    'category': '',
+    'sequence': 23,
+    'summary': 'Able Pack Inventory',
+    'version': '1.0',
+    'description': """
+    """,
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/stock_picking_views.xml',
+    ],
+    'assets': {
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'LGPL-3',
+}
