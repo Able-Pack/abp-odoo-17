8a488e5e3b36925f10ac308f80baf559b7516bfb
victorimannuel <victorimannuel.dev@gmail.com>
2024-11-08 23:01:58 +0700

    fix(barcode): ensure 2 decimal places for price

--- addons/abp_utils/utils.py
+++ addons/abp_utils/utils.py
@@ -1,4 +1,6 @@
 def format_currency_amount(amount, currency_id):
         pre = currency_id.position == 'before'
         symbol = u'{symbol}'.format(symbol=currency_id.symbol or '')
-        return u'{pre}{0}{post}'.format(amount, pre=symbol if pre else '', post=symbol if not pre else '')
+        # Format amount to two decimal places
+        formatted_amount = '{:,.2f}'.format(amount)
+        return u'{pre}{0}{post}'.format(formatted_amount, pre=symbol if pre else '', post=symbol if not pre else '')
