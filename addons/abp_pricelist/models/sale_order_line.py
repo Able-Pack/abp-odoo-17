0afa75b56c8fc50667c34e9738df3967b4200f96
victorimannuel <victorimannuel.dev@gmail.com>
2024-11-10 19:51:26 +0700

    refactor(pricelist): move code to abp_pricelist module for better structure

--- addons/abp_pricelist/models/sale_order_line.py
+++ addons/abp_pricelist/models/sale_order_line.py
@@ -4,8 +4,9 @@ from odoo import api, fields, models, _
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     
+    customer_product_ref = fields.Char(string='Customer Product Ref')
     barcode = fields.Char(string='EAN13')
-    retail_price = fields.Monetary()
+    retail_price = fields.Monetary(string='Retail Price')
     
     # Override parent method
     @api.depends('product_id', 'product_uom', 'product_uom_qty')
@@ -13,6 +14,7 @@ class SaleOrderLine(models.Model):
         for rec in self:
             super()._compute_pricelist_item_id()
             if rec.pricelist_item_id:
+                rec.customer_product_ref = rec.pricelist_item_id.customer_product_ref
                 rec.barcode = rec.pricelist_item_id.barcode
                 rec.retail_price = rec.pricelist_item_id.retail_price or rec.pricelist_item_id.fixed_price
                 
