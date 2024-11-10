fe762c989506c9a623f52f29343455b3be82e831
victorimannuel <victorimannuel.dev@gmail.com>
2024-11-11 04:43:22 +0700

    feat(sale order): set pricelist's field to computed
    - and commit leftover file for previous commit

--- addons/abp_pricelist/models/sale_order_line.py
+++ addons/abp_pricelist/models/sale_order_line.py
@@ -4,15 +4,17 @@ from odoo import api, fields, models, _
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     
-    customer_product_ref = fields.Char(string='Customer Product Ref')
-    barcode = fields.Char(string='EAN13')
-    retail_price = fields.Monetary(string='Retail Price')
+    customer_product_ref = fields.Char(string='Customer Product Ref', compute='_compute_pricelist_item_id')
+    barcode = fields.Char(string='EAN13', compute='_compute_pricelist_item_id')
+    retail_price = fields.Float(string="Retail Price", compute='_compute_pricelist_item_id')
     
     # Override parent method
     @api.depends('product_id', 'product_uom', 'product_uom_qty')
     def _compute_pricelist_item_id(self):
         for rec in self:
             super()._compute_pricelist_item_id()
+            rec.customer_product_ref = rec.barcode = False
+            rec.retail_price = 0.0
             if rec.pricelist_item_id:
                 rec.customer_product_ref = rec.pricelist_item_id.customer_product_ref
                 rec.barcode = rec.pricelist_item_id.barcode
