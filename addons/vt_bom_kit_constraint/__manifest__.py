fd9b3f1be91aecff1a6425ea7e8ae9357d6927e9
victorimannuel <victorimannuel.dev@gmail.com>
2024-11-30 22:05:44 +0700

    feat: new module to add bom kit transfer restriction feature

---
+++ addons/vt_bom_kit_constraint/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "VT Bom Kit Constraint",
+    "summary": "Avoid to deliver a kit partially",
+    "version": "17.0",
+    "category": "Stock",
+    "website": "https://github.com/OCA/manufacture",
+    "author": "Victor Imannuel (Child of God)",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "mrp"
+    ],
+    "data": [
+        "views/stock_picking_views.xml"
+    ],
+}
