a948c18c707516887cf0b4daca5a9e7160e7a32c
victorimannuel <victorimannuel.dev@gmail.com>
2024-12-03 02:14:27 +0700

    feat(sale order): handle duplicated pricelist item
    - fill customer product ref and barcode value with 'Multiple pricelist item'
    - fill retail price with 0.0
    - show validation error pop up if there is any duplicated pricelist item in the line when about to print the barcode

--- addons/abp_pricelist/models/sale_order_line.py
+++ addons/abp_pricelist/models/sale_order_line.py
@@ -15,8 +15,17 @@ class SaleOrderLine(models.Model):
             super()._compute_pricelist_item_id()
             rec.customer_product_ref = rec.barcode = False
             rec.retail_price = 0.0
+            
             if rec.pricelist_item_id:
                 rec.customer_product_ref = rec.pricelist_item_id.customer_product_ref
                 rec.barcode = rec.pricelist_item_id.barcode
                 rec.retail_price = rec.pricelist_item_id.retail_price or rec.pricelist_item_id.fixed_price
                 
+            # Manually search to product.pricelist.item to make sure if there is only single pricelist item record
+            pricelist_item_temp = self.env['product.pricelist.item'].search([
+                ('pricelist_id', '=', rec.order_id.partner_id.property_product_pricelist.id),
+                ('product_tmpl_id', '=', rec.product_template_id.id)
+            ])
+            if len(pricelist_item_temp) > 1:
+                rec.customer_product_ref = rec.barcode = 'Multiple pricelist item'
+                rec.retail_price = 0.0
