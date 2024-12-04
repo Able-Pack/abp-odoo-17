0ca28acf26be45bca6ef323cf04071259f9d6502
victorimannuel <victorimannuel.dev@gmail.com>
2024-12-05 00:53:03 +0700

    feat(customer catalogue): auto update customer catalogue values from pricelist upon pricelist item creation

--- addons/abp_customer_catalogue/models/pricelist.py
+++ addons/abp_customer_catalogue/models/pricelist.py
@@ -1,17 +1,31 @@
-from odoo import models, _
+from odoo import api, models, _
 
 
 class ProductPricelistItem(models.Model):
     _inherit = 'product.pricelist.item'
     
+    @api.model
+    def create(self, vals):
+        # Trigger recompute on linked customer.catalogue records
+        # Use filtered function because non-stored field can't be searched using odoo ORM (property_product_pricelist)
+        matching_partners = self.env['res.partner'].search([]).filtered(lambda x: x.property_product_pricelist.id == vals['pricelist_id']).ids
+        related_catalogues = self.env['customer.catalogue'].search([
+            ('partner_id', 'in', matching_partners),
+            ('product_tmpl_id.id', '=', vals['product_tmpl_id'])
+        ])
+        for catalogue in related_catalogues:
+            catalogue.barcode = vals.get('barcode', False)
+            catalogue.customer_product_ref = vals.get('customer_product_ref', False)
+            
+        return super().create(vals)
     
     def write(self, vals):
         res = super().write(vals)
-        # if 'customer_product_ref' in vals or 'barcode' in vals or 'retail_price' in vals:
         # Trigger recompute on linked customer.catalogue records
         related_catalogues = self.env['customer.catalogue'].search([
             ('pricelist_item_id', 'in', self.ids)
         ])
-        related_catalogues._compute_pricelist_item()
+        if related_catalogues:
+            related_catalogues._compute_pricelist_item()
         
         return res
