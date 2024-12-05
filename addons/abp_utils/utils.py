57cfae6d910bf12bbae9a5f5e16827c08cbc3353
victorimannuel <victorimannuel.dev@gmail.com>
2024-12-06 03:02:23 +0700

    fix(customer statement): modify report
    - add sum total to total all amount of each partners (if more than 1 partners selected)
    - modify format currency amount in abp_utils to adapt if no currency provided, and adjust space before currency symbol

--- addons/abp_utils/utils.py
+++ addons/abp_utils/utils.py
@@ -1,6 +1,9 @@
 def format_currency_amount(amount, currency_id):
-        pre = currency_id.position == 'before'
-        symbol = u'{symbol}'.format(symbol=currency_id.symbol or '')
-        # Format amount to two decimal places
-        formatted_amount = '{:,.2f}'.format(amount)
-        return u'{pre}{0}{post}'.format(formatted_amount, pre=symbol if pre else '', post=symbol if not pre else '')
+	# Format amount to two decimal places
+    formatted_amount = '{:,.2f}'.format(amount)
+    if not currency_id:
+        return formatted_amount
+    
+    pre = currency_id.position == 'before'
+    symbol = u'{symbol}'.format(symbol=currency_id.symbol or '')
+    return u'{pre} {0}{post}'.format(formatted_amount, pre=symbol if pre else '', post=symbol if not pre else '')
