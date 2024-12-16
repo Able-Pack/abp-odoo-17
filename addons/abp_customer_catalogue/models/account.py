7066d89da03e24c8734a2575a0b7e929be917409
victorimannuel <victorimannuel.dev@gmail.com>
2024-12-17 00:35:10 +0700

    fix(customer catalogue): change trigger new catalogue record on invoice confirmation
    - before: trigger on invoice creation
    - after: trigger on invoice confirmation

--- addons/abp_customer_catalogue/models/account.py
+++ addons/abp_customer_catalogue/models/account.py
@@ -1,7 +1,8 @@
 import json
 from lxml import etree
-from odoo import api, fields, models
+from odoo import _, api, fields, models
 from odoo.addons.abp_utils import views as utils
+from odoo.exceptions import UserError
 
 
 class AccountMove(models.Model):
@@ -11,6 +12,8 @@ class AccountMove(models.Model):
     show_admin_product = fields.Boolean(string='Show admin products?')
     show_base_product = fields.Boolean(string='Show base products?', default=False)
     show_customer_specific_product = fields.Boolean(string='Show customer-specific products?', default=False)
+    has_new_customer_catalogue = fields.Boolean(default=False)
+    new_customer_catalogue_json = fields.Json()
     
     @api.model
     def get_view(self, view_id=None, view_type="form", **options):
@@ -48,6 +51,105 @@ class AccountMove(models.Model):
                 
         return super().write(vals)
     
+    def _create_customer_catalogue(self):
+        result = []
+        products = self.invoice_line_ids.mapped('product_id.product_tmpl_id')
+        draft_new_customer_catalogue = []
+        
+        for product in products:
+            customer_catalogue_ids = self.env['customer.catalogue'].search([
+                ('product_tmpl_id', '=', product.id),
+                ('partner_id', '=', self.partner_id.id),
+            ])
+            if not customer_catalogue_ids:
+                values = {
+                    'partner_id': self.partner_id.id,
+                    'product_id': product.product_variant_ids[0].id
+                }
+                draft_new_customer_catalogue.append(values)
+                # create new customer catalogue record
+                self.env['customer.catalogue'].create(values)
+                # include product template name and price unit in the email but not for the customer catalogue values
+                values.update({
+                    'barcode': False,
+                    'product_tmpl_name':product.display_name,
+                    'price_unit': False
+                })
+                result.append(values)
+        
+        if result:
+            self.has_new_customer_catalogue = True
+            self.new_customer_catalogue_json = result
+        
+        return result
+    
+    @api.model
+    def _notify_catalogue_creation(self, new_customer_catalogues, partner_id):
+        subject = 'Customer Catalogue: New Records'
+        
+        body_html = f"""
+            <p>Hello,</p>
+            <p>A new customer catalogue for partner <strong>{partner_id.name}</strong> has been created with the following details:</p>
+            
+            <table border="1" cellpadding="5" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Product</th>
+                        <th>EAN13</th>
+                        <th>Customer Product Code</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+        """
+        
+        # Add each item to the table
+        for idx, item in enumerate(new_customer_catalogues):
+            body_html += f"""
+                <tr>
+                    <td style="text-align: center;">{idx+1}</td>
+                    <td>{item.get('product_tmpl_name', '-')}</td>
+                    <td>{item.get('barcode', '-')}</td>
+                    <td>{item.get('customer_product_code', '-')}</td>
+                    <td>{item.get('price_unit', '-')}</td>
+                </tr>
+            """
+        
+        body_html += """
+                </tbody>
+            </table>
+            <br/>
+            <p>Best regards,
+            <br/>Able Pack Ltd</p>
+        """
+        
+        email_to = self.env["ir.config_parameter"].sudo().get_param("abp_customer_catalogue.newly_created_customer_catalogue_target_email")
+        outgoing_mail_server = self.env['ir.mail_server'].sudo().search([], order='sequence', limit=1)
+        # Create the mail.message record
+        mail_values = {
+            'subject': subject,
+            'body_html': body_html,
+            'email_to': email_to,
+            'email_from': outgoing_mail_server.smtp_user or '',
+        }
+        mail_id = self.env['mail.mail'].sudo().create(mail_values)
+        
+        try:
+            # Send the email
+            mail_id.send()
+            # After sending, set the boolean field back to False
+            self.has_new_customer_catalogue = False
+        except Exception as e:
+            raise UserError(_(str(e)))
+        
+    def action_post(self):
+        res = super().action_post()
+        # Create new customer catalogue if exist and notify through email
+        self._create_customer_catalogue()
+        if self.has_new_customer_catalogue:
+            self._notify_catalogue_creation(self.new_customer_catalogue_json, self.partner_id)
+        return res
     
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
