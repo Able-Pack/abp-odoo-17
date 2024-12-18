a58be6c4a402927d42be77bc5ad163d9f2c8a6ca
victorimannuel <victorimannuel.dev@gmail.com>
2024-12-18 07:10:42 +0700

    fix(accounting): change partner_parent_id string

--- addons/abp_accounting/models/account_move_line.py
+++ addons/abp_accounting/models/account_move_line.py
@@ -7,6 +7,6 @@ class AccountMoveLine(models.Model):
     partner_parent_id = fields.Many2one(
         comodel_name='res.partner',
         related='partner_id.parent_id',
-        string="Partner's Parent",
+        string="Parent",
         store=True
     )
