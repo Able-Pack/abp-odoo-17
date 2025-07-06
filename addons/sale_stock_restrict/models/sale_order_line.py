414cc3e2437fd49600d81f39d34063436793cfd7
Victor Imannuel <135267860+victorimannuel@users.noreply.github.com>
2025-07-06 19:46:51 +0700

    [FIX] sale_stock_restrict: Add condition on validation and add additional protection in write function (#257)

--- addons/sale_stock_restrict/models/sale_order_line.py
+++ addons/sale_stock_restrict/models/sale_order_line.py
@@ -9,5 +9,12 @@ class SaleOrderLine(models.Model):
     
     @api.onchange('product_uom_qty')
     def _onchange_product_uom_qty(self):
-        if self.product_uom_qty > self.product_qty_available:
+        if self.product_id and self.product_uom_qty > 0 and self.product_uom_qty > self.product_qty_available:
             raise ValidationError(_('Quantity cannot be greater than on-hand quantity.'))
+    
+    def write(self, vals):
+        if 'product_uom_qty' in vals:
+            for line in self:
+                if line.product_id and vals['product_uom_qty'] > line.product_qty_available:
+                    raise ValidationError(_('Quantity cannot be greater than on-hand quantity.'))
+        return super(SaleOrderLine, self).write(vals)
